08a38c359d9ff7c2b527a0531f19b432561b77d4
Murshed Majdy Qatoseh <morshedmorahed01@gmail.com>
2022-08-30 11:48:33 +0300

    lab06

---
+++ src/App.js
@@ -0,0 +1,170 @@
+import './App.css';
+import Header from './components/Header';
+import Image from './components/Image';
+import Weather from './components/weather.js';
+import Movie from './components/movie.js';
+import Error from './components/error.js';
+import React from 'react';
+import axios from 'axios';
+import { Button, Row, Col } from 'react-bootstrap';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: "",
+      cityData: {},
+      cityName: "",
+      lat: 0,
+      lon: 0,
+      error: false,
+      errorMessage: "",
+      errorWeather: false,
+      errorWeatherMessage: "",
+      errorMovie: false,
+      errorMovieMessage: "",
+      mapData: {},
+      mapUrl: "",
+      showCity: false,
+      forecast: [],
+      showForecast: false,
+      movieData: [],
+      showMovie: false,
+      movieImg: {},
+    };
+  }
+
+  submitCityHandler = async (event) => {
+    event.preventDefault();
+    this.resetState();
+
+    let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.city}&format=json`;
+    try {
+      let cityInfo = await axios.get(url).then(res => res.data);
+      this.state.cityData = cityInfo[0];
+      this.state.cityName = this.state.cityData.display_name.split(',')[0];
+
+      let mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${cityInfo[0].lat},${cityInfo[0].lon}&zoom=12`;
+      this.setState({
+        mapUrl: mapUrl,
+        showCity: true,
+      })
+    } catch (error) {
+      let errorString = (error.name + ': ' + error.message);
+      this.setState({
+        errorMessage: errorString,
+        error: true,
+      })
+    }
+
+    try {
+      let weatherUrl = `${process.env.REACT_APP_SERVER}/weather?lat=${this.state.cityData.lat}&lon=${this.state.cityData.lon}`;
+      let results = await axios.get(weatherUrl);
+      if(results.data === ''){
+        throw new Error('Error with data.');
+      }
+      let forecastArr = results.data;
+      this.setState({
+        forecast: forecastArr,
+        showForecast: true
+      })
+    } catch (error) {
+      let errorString = (error.name + ': ' + error.message);
+      this.setState({
+        errorWeatherMessage: errorString,
+        errorWeather: true,
+      })
+    }
+
+    try {
+      let movieUrl = `${process.env.REACT_APP_SERVER}/movie?keyword=${this.state.cityName}`;
+      let movieResults = await axios.get(movieUrl);
+      let resultData = movieResults.data;
+      this.setState({
+        movieData: resultData,
+        showMovie: true,
+      })
+    } catch (error) {
+      let errorString = (error.name + ': ' + error.message);
+      this.setState({
+        errorMovie: true,
+        errorMovieMessage: errorString,
+      })
+    }
+  }
+
+  HandleCityInput = (event) => {
+    this.setState({
+      city: event.target.value,
+    });
+    if (event.target.value === '') {
+      this.resetState();
+      this.render();
+    }
+    console.log(this.state.city);
+  }
+
+  resetState() {
+    this.setState({
+      cityData: {},
+      error: false,
+      errorMessage: "",
+      errorWeather: false,
+      errorWeatherMessage: "",
+      errorMovie: false,
+      errorMovieMessage: "",
+      mapData: {},
+      mapUrl: "",
+      showCity: false,
+      showForecast: false,
+      showMovie: false,
+      movieData: [],
+    });
+  }
+
+  render() {
+    let cityDisplay = this.state.showCity ?
+      <>
+        <ul style={{ listStyleType: "none" }}>
+          <li>{this.state.cityData.display_name}</li>
+          <li>Latitude: {this.state.cityData.lat}</li>
+          <li>Longitude: {this.state.cityData.lon}</li>
+        </ul>
+        <Image src={this.state.mapUrl} alt="A map of the selected city." />
+      </> : this.state.error ? <Error errorMessage={this.state.errorMessage} /> : <></>;
+
+    let forecastDisplay = this.state.showForecast ?
+      <Weather forecast={this.state.forecast} /> : this.state.error ? <Error errorMessage={this.state.errorWeatherMessage} /> : <></>;
+
+    let movieDisplay = this.state.showMovie ? <Movie movieData={this.state.movieData} /> : this.state.errorMovie ? <Error errorMessage={this.state.errorMovieMessage} /> : <></>;
+
+    return (
+      <>
+        <Header cityData={this.state.cityData} />
+        <main>
+          <form id="form" onSubmit={this.submitCityHandler}>
+            <label>Enter a City</label><br />
+            <input type="text" onChange={this.HandleCityInput}></input><br />
+            <Button style={{margin: '1%'}} type="submit">Explore!</Button>
+            <div>
+              <div style={{marginBottom: '2%'}}>{cityDisplay}</div>
+              
+              <Row>
+                <Col style={{margin: '1%s'}}>
+                  {forecastDisplay}
+                </Col>
+                <Col>
+                  {movieDisplay}
+                </Col>
+
+              </Row>
+
+            </div>
+          </form>
+        </main>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/weather.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { Card, ListGroup } from 'react-bootstrap';
+
+
+class Weather extends React.Component {
+
+    render() {
+        let listItems = this.props.forecast.data.map((val, index) => {
+            let forecastDay =
+                <ListGroup>
+                    <ListGroup.Item style={{ fontSize: "20px" }}>
+                        {val.datetime}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        Temp: {val.temp}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        High: {val.high_temp}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        Low: {val.low_temp}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        Description: {val.weather.description}
+                    </ListGroup.Item>
+                </ListGroup>
+            return <ListGroup.Item style={{ borderStyle: "groove" }} key={index}>{forecastDay}</ListGroup.Item>;
+        });
+        let forecastList = <ListGroup>{listItems}</ListGroup>;
+        return (
+            <Card>
+                <Card.Header>Forecast</Card.Header>
+                <Card.Body>
+                    {forecastList}
+                </Card.Body>
+            </Card>
+        )
+    }
+}
+
+export default Weather;
